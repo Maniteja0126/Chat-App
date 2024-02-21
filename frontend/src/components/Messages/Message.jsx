c892491d1a3abff83d482c97fdde9221b3551914
Maniteja0126 <maniteja2601@gmail.com>
2024-02-21 18:20:41 +0530

    fix : fixed the bug in Message Container

--- frontend/src/components/Messages/Message.jsx
+++ frontend/src/components/Messages/Message.jsx
@@ -9,7 +9,7 @@ const Message = ({ message }) => {
   const { selectedConversation } = useConversation()
   const fromMe = message.senderId === authUser._id
   const formattedTime = extractTime(message.createdAt)
-  const chatClassName = fromMe ? 'chat-start' : 'chat-end'
+  const chatClassName = fromMe ? 'chat-end' : 'chat-start'
   const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic
   const bubbleBgColor = fromMe ? 'bg-blue-500' : ""
   const shakeClass = message.shouldShake ? 'shake' : ""
